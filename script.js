8b08a5209a09cade61322d31c150f9950920cb15
Nusrat Jahan <nusratjahanitbd1@gmail.com>
2025-02-09 23:06:41 +0600

    Javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+let marks;
+
+marks = 39;
+
+
+
+ if(marks>=60){ 
+    console.log("The ticket price:$8");
+}
+
+else if(marks>=18){ 
+    console.log("The ticket price:$15");
+}
+
+else if(marks>=5){ 
+    console.log("The ticket price:$10");
+}
+
+else if(marks<5){
+    console.log("The ticket is free");
+}
